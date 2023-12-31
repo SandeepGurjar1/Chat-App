075832f32f2c6227411a9a35c618f7bc4a1d274a
Sandeep Gurjar <101051507+SandeepGurjar1@users.noreply.github.com>
2023-12-31 18:18:41 +0530

    Add files via upload

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "https://chat-app-vfk7.onrender.com/";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
